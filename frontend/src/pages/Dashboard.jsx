c824e7d2922c5884a58e73c3e875badf0f37459f
shinnosuke-noda <shinnosuke.noda@mail.utoronto.ca>
2025-05-23 15:46:29 -0400

    Added the goal function.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,8 @@ function Dashboard() {
     const navigate = useNavigate();
 
     const [goals, setGoals] = useState([]);
+    const [goalText, setGoalText] = useState("");
+    const [showInput, setShowInput] = useState(false);
 
     const signOut = async () => {
         const { error } = await supabase.auth.signOut();
@@ -14,8 +16,30 @@ function Dashboard() {
         navigate("/login");
     };
 
-    function handleAddCard() {
-        setGoals([...goals, <Goal />]);
+    const handleAddGoal = () => {
+        setShowInput(true);
+    };
+
+    const handleCreateGoal = (e) => {
+        e.stopPropagation();
+        setShowInput(false);
+
+        // If the user didn't type anything, it just closes the box.
+        if (goalText) {
+            setGoals([...goals, <Goal />]);
+        }
+
+        setGoalText("");
+    }
+
+    const handleInputChange = (e) => {
+        setGoalText(e.target.value);
+    }
+
+    const handleCancelGoal = (e) => {
+        // This stops calling the parent element.
+        e.stopPropagation();
+        setShowInput(false);
     };
 
     return (
@@ -24,14 +48,41 @@ function Dashboard() {
             <div className="container">
                 <div
                     className="create-goal-container"
-                    onClick={handleAddCard}
+                    onClick={handleAddGoal}
                 >
-                    + Add a goal
+                    {showInput ? (
+                        <>
+                            <input
+                                className="input-field"
+                                type="text"
+                                value={goalText}
+                                onChange={handleInputChange}
+                                placeholder="Type your goal"
+                                maxLength="50"
+                            />
+                            <br></br>
+                            <button
+                                className="add-goal-button"
+                                onClick={handleCreateGoal}
+                            >
+                                Add
+                            </button>
+                            <button
+                                className="cancel-goal-button"
+                                onClick={handleCancelGoal}
+                            >
+                                X
+                            </button>
+                        </>
+                    ) : (
+                        <>+ Add a goal</>
+                    )}
                 </div>
                 {goals.map((goal, index) => (
-                    <React.Fragment key={index}>{goal}</React.Fragment>
+                    <React.Fragment key={index}>
+                        {goal}
+                    </React.Fragment>
                 ))}
-                
             </div>
             <button
                 className="signout-button"
